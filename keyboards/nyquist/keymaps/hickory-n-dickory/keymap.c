e32a2c254d07825e11c7a37f01fde1ad3232bdf6
HokieGeek <andres.f.perez@gmail.com>
2018-07-13 15:48:29 -0400

    Macros!!

--- keyboards/nyquist/keymaps/hickory-n-dickory/keymap.c
+++ keyboards/nyquist/keymaps/hickory-n-dickory/keymap.c
@@ -18,6 +18,8 @@ enum custom_keycodes {
     LOWER,
     RAISE,
     ADJUST,
+    TMUX_NEXT,
+    TMUX_PREV,
 };
 
 // Fillers to make layering more clear
@@ -44,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,      KC_O,      KC_P,    KC_ENT, \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,      KC_L,      KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH, KC_RSFT, \
-  ADJUST,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT,   KC_DOWN, KC_UP,   KC_RGHT \
+  ADJUST,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   TMUX_NEXT, TMUX_PREV, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
@@ -189,6 +191,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 layer_on(_ADJUST);
             } else {
                 layer_off(_ADJUST);
+            }
+            return false;
+            break;
+
+        // MACROS
+        case TMUX_NEXT:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTRL("a")"n");
+            }
+            return false;
+            break;
+
+        case TMUX_PREV:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTRL("a")"p");
             }
             return false;
             break;
