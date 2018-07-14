3493462f283f72217d97880d6fad188efbf1f67d
HokieGeek <andres.f.perez@gmail.com>
2018-07-13 23:09:31 -0400

    Dynamic macro key

--- keyboards/nyquist/keymaps/hickory-n-dickory/keymap.c
+++ keyboards/nyquist/keymaps/hickory-n-dickory/keymap.c
@@ -20,7 +20,11 @@ enum custom_keycodes {
     ADJUST,
     TMUX_NEXT,
     TMUX_PREV,
+
+    DYNAMIC_MACRO_RANGE,
 };
+
+#include "dynamic_macro.h"
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -36,17 +40,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * | ShEnt|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      | Alt  | GUI  |Lower |Space |Space |Raise | TX_N | TX_P | Mute |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
-  KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,      KC_9,      KC_0,    KC_BSPC, \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,      KC_O,      KC_P,    KC_ENT, \
-  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,      KC_L,      KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH, KC_RSFT, \
-  _______,  _______, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   TMUX_NEXT, TMUX_PREV, KC_MUTE, _______ \
+  KC_GESC,    KC_1,            KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,      KC_9,      KC_0,    KC_BSPC, \
+  KC_TAB,     KC_Q,            KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,      KC_O,      KC_P,    KC_ENT, \
+  KC_LCTL,    KC_A,            KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,      KC_L,      KC_SCLN, KC_QUOT, \
+  KC_SFTENT,  KC_Z,            KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH, KC_RSPC, \
+  _______,    DYN_MACRO_PLAY1, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   TMUX_NEXT, TMUX_PREV, KC_MUTE, _______ \
 ),
 
 /* Lower
@@ -59,15 +63,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Enter |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      | Vol- |      |
+ * |      |MSTOP |      |      |      |             |      |      |      | Vol- |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_TILD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
-  KC_BSPC, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
-  KC_CAPS, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_PIPE, \
-  KC_ENT,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, _______ \
+  KC_TILD, _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
+  KC_BSPC, _______,      _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
+  KC_CAPS, _______,      _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_PIPE, \
+  KC_ENT,  _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, DYN_REC_STOP, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, _______ \
 ),
 
 /* Raise
@@ -80,15 +84,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      | Vol- | Vol+ | Mute |
+ * |      | MREC |      |      |      |             |      |      | Vol- | Vol+ | Mute |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_GRV,  _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
-  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_BSLS, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, _______ \
+  KC_F1,   KC_F2,          KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+  KC_GRV,  _______,        _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
+  _______, _______,        _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_BSLS, \
+  _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, DYN_REC_START1, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, _______ \
 ),
 
 /* Colemak
@@ -142,6 +146,10 @@ void persistent_default_layer_set(uint16_t default_layer) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_dynamic_macro(keycode, record)) {
+        return false;
+    }
+
     switch (keycode) {
         case QWERTY:
             if (record->event.pressed) {
