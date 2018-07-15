4ace10fbcf488cf9808ff80c01bb513e1d32445f
HokieGeek <andres.f.perez@gmail.com>
2018-07-14 23:37:20 -0400

    Swap hands works as advertised, now

--- keyboards/nyquist/keymaps/hickory-n-dickory/config.h
+++ keyboards/nyquist/keymaps/hickory-n-dickory/config.h
@@ -20,13 +20,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* Use I2C or Serial, not both */
 #define USE_SERIAL
 
-/* Select hand configuration */
 #define MASTER_LEFT
+// #define EE_HANDS
 
-#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+
+#define TAPPING_TERM 150
 
 #define TAPPING_TOGGLE 2
 
