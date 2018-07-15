d1d3977d0c54b69609adc5c334847e9f22c328cf
HokieGeek <andres.f.perez@gmail.com>
2018-07-14 22:37:19 -0400

    Trying out sticky layers

--- keyboards/nyquist/keymaps/hickory-n-dickory/keymap.c
+++ keyboards/nyquist/keymaps/hickory-n-dickory/keymap.c
@@ -42,15 +42,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | ShEnt|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      | Alt  | GUI  |Lower |Space |Space |Raise | TX_N | TX_P | Mute |      |
+ * | SWAP | MACR | Alt  | GUI  |Lower |Space |Space |Raise | TX_N | TX_P | Mute | SWAP |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
   KC_GESC,      KC_1,            KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,      KC_9,      KC_0,    KC_BSPC, \
   KC_TAB,       KC_Q,            KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,      KC_O,      KC_P,    KC_ENT, \
   KC_LCTL,      KC_A,            KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,      KC_L,      KC_SCLN, KC_QUOT, \
-  KC_SFTENT,    KC_Z,            KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH, KC_RSFT, \
-  SH_TT,        DYN_MACRO_PLAY1, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   TMUX_NEXT, TMUX_PREV, KC_MUTE, SH_TT \
+  KC_LSFT,    KC_Z,            KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH, KC_RSFT, \
+  SH_TT,        DYN_MACRO_PLAY1, KC_LALT, KC_LGUI, TT(LOWER),   KC_SPC,  KC_SPC,  TT(RAISE),   TMUX_NEXT, TMUX_PREV, KC_MUTE, SH_TT \
 ),
 
 /* Lower
@@ -63,15 +63,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Enter |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |MSTOP |      |      |      |             |      |      |      | Vol- |      |
+ * |      |MSTOP |      |      |      |      |      |      |      |      | Vol- |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
   KC_TILD, _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
   KC_BSPC, _______,      _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
   KC_CAPS, _______,      _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_PIPE, \
-  KC_ENT,  _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  SH_TG, DYN_REC_STOP, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, SH_TG \
+  _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  SH_TG, DYN_REC_STOP,   _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, SH_TG \
 ),
 
 /* Raise
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | MREC |      |      |      |             |      |      | Vol- | Vol+ | Mute |
+ * |      | MREC |      |      |      |      |      |      |      |      | Vol+ |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT( \
