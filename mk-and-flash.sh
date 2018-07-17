556c4bef8b13124911400b310a2c9e801d2c105c
HokieGeek <andres.f.perez@gmail.com>
2018-07-17 11:24:19 -0400

    Ok, I have accents on mac

--- mk-and-flash.sh
+++ mk-and-flash.sh
@@ -1,1 +1,1 @@
- make nyquist/rev2:hickory-n-dickory && sudo make nyquist/rev2:hickory-n-dickory:avrdude
+ make EXTRAFLAGS=-DTARGET_MAC nyquist/rev2:hickory-n-dickory && sudo make EXTRAFLAGS=-DTARGET_MAC nyquist/rev2:hickory-n-dickory:avrdude
