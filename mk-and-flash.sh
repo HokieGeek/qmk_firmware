978c84eb5cd83ae5e138d9f932f00b10bdec9981
HokieGeek <andres.f.perez@gmail.com>
2018-07-17 23:23:59 -0400

    Fixed build script

--- mk-and-flash.sh
+++ mk-and-flash.sh
@@ -1,1 +1,2 @@
- make EXTRAFLAGS=-DTARGET_MAC nyquist/rev2:hickory-n-dickory && sudo make EXTRAFLAGS=-DTARGET_MAC nyquist/rev2:hickory-n-dickory:avrdude
+sudo make EXTRAFLAGS=-DTARGET_MAC nyquist/rev2:hickory-n-dickory:avrdude
+# sudo make EXTRAFLAGS=-DTARGETLINUX nyquist/rev2:hickory-n-dickory:avrdude
