78dc704eb8df4c28d4d28c4c3e801c4cec16c4b1
HokieGeek <andres.f.perez@gmail.com>
2018-09-03 15:36:24 -0400

    Split the encoder logic out

--- users/hokiegeek/encoder.c
+++ users/hokiegeek/encoder.c
@@ -1,30 +1,44 @@
-#ifndef ENCODER_RESOLUTION
-  #define ENCODER_RESOLUTION 4
-#endif
+#ifndef USERSPACE_ENCODER
+#define USERSPACE_ENCODER
+
+#include "quantum.h"
+#include "encoder.h"
 
 // Encoder
 static uint8_t  encoder_state  = 0;
-static int8_t  encoder_value  = 0;
-static int8_t  encoder_LUT[]  = { 0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0 };
+static int8_t   encoder_value  = 0;
+static int8_t   encoder_LUT[]  = { 0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0 };
 
 __attribute__ ((weak))
 void encoder_update(bool clockwise) { }
 
-void check_encoder() {
-    encoder_state <<= 2;
-    encoder_state |= (palReadPad(GPIOB, 12) << 0) | (palReadPad(GPIOB, 13) << 1);
-    encoder_value += encoder_LUT[encoder_state & 0xF];
+void check_encoder(void) {
+    uint8_t pinB = (PINB & (1 << ENC_B)) >> 1;
+    uint8_t pinA = (PINF & (1 << ENC_A)) >> 4;
 
-    // int val = encoder_value;
-    if (encoder_value <= -ENCODER_RESOLUTION) { // direction is arbitrary here, but this clockwise
-        encoder_update(0);
-        // return 1;
+    encoder_state <<= 2;                   //remember previous state
+    encoder_state |= ( (pinB) | (pinA) );  //add current state
+    encoder_value += encoder_LUT[( encoder_state & 0x0f )];
+
+    if (encoder_value <= -ENCODER_RESOLUTION) {
+        encoder_update(false);
     }
     if (encoder_value >= ENCODER_RESOLUTION) {
-        encoder_update(1);
-        // return -1;
+        encoder_update(true);
     }
     encoder_value %= ENCODER_RESOLUTION;
-
-    // return 0
 }
+
+void encoder_init(void) {
+    // TODO: use this if nyquist
+    // JTAG disable for PORT F. write JTD bit twice within four cycles.
+    MCUCR |= (1<<JTD);
+    MCUCR |= (1<<JTD);
+
+    DDRB &= ~(1 << ENC_B);
+    DDRF &= ~(1 << ENC_A);
+    PORTB |= (1 << ENC_B);
+    PORTF |= (1 << ENC_A);
+}
+
+#endif // USERSPACE_ENCODER
