0d03d479b7952c894f50d9620abd0bea214caee4
HokieGeek <andres.f.perez@gmail.com>
2018-09-16 22:12:22 -0400

    A bunch of tweaks as I remap now that I have a working encoder

--- users/hokiegeek/encoder.h
+++ users/hokiegeek/encoder.h
@@ -1,3 +1,6 @@
+#ifndef USERSPACE_ENCODER
+#define USERSPACE_ENCODER
+
 #ifndef ENCODER_RESOLUTION
   #define ENCODER_RESOLUTION 4
 #endif
@@ -5,6 +8,10 @@
 #define ENC_A PF4
 #define ENC_B PB2
 
+enum { td_encoder = 0 };
+
+#define ENCODER_TD_ENTRY [td_encoder] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, encoder_td_actions, NULL, 400)
+
 #define KC_ENC TD(td_encoder)
 
-enum { td_encoder = 0 };
+#endif // USERSPACE_ENCODER
