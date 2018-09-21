b939276b732daea7eda1c917ea24ee48c00f4cef
HokieGeek <andres.f.perez@gmail.com>
2018-09-20 21:17:59 -0400

    Getting better functionality out of the tmux tapdance

--- users/hokiegeek/tmux.c
+++ users/hokiegeek/tmux.c
@@ -1,24 +1,22 @@
 #include "tmux.h"
 
+#include "hokiegeek.h"
+
 static int td_tmux_state = 0;
 
 void td_tmux_finished(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        if (state->interrupted || !state->pressed) {
-            SEND_TMUX("n");
-            td_tmux_state = SINGLE;
-        } else {
-            SEND_TMUX(SS_LCTRL("a"));
-            td_tmux_state = HOLD;
-        }
-    } else if (state->count == 2) {
-        SEND_TMUX(SS_LCTRL("a"));
-        td_tmux_state = DOUBLE;
+    td_tmux_state = process_td_state(state, user_data);
+
+    switch (td_tmux_state) {
+        case SINGLE:      SEND_TMUX("n"); break;
+        case SINGLE_HOLD: SEND_TMUX(""); break;
+        case DOUBLE:      SEND_TMUX(SS_LCTRL("a")); break;
+        case DOUBLE_HOLD: SEND_TMUX(SS_LCTRL("a")); break;
     }
 }
 
 void td_tmux_reset(qk_tap_dance_state_t *state, void *user_data) {
-    if (td_tmux_state == HOLD) {
+    if (td_tmux_state == DOUBLE_HOLD) {
         SEND_TMUX(SS_LCTRL("a"));
     }
     td_tmux_state = 0;
@@ -26,7 +24,6 @@ void td_tmux_reset(qk_tap_dance_state_t *state, void *user_data) {
 
 bool tmux_process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        // TMUX
         case TMUX_NEXT:
         case TMUX_PREV:
         case TMUX_LAST:
