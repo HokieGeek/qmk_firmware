95498321cdeaf43dc1d135df63a6e5bbae80efa5
HokieGeek <andres.f.perez@gmail.com>
2018-09-22 11:25:28 -0400

    Using defines to abstract the userspace tapdances

--- users/hokiegeek/encoder.h
+++ users/hokiegeek/encoder.h
@@ -17,7 +17,7 @@ typedef struct {
 
 enum { td_encoder = 0 };
 
-#define ENCODER_TD_ENTRY [td_encoder] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, encoder_td_actions, NULL, 400)
+#define TD_ENCODER_ENTRY [td_encoder] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, encoder_td_actions, NULL, 300)
 
 #define KC_ENC TD(td_encoder)
 
