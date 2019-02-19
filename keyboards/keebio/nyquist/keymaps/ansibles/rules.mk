74ea2e70a4168ba59004d79ccfcd2b4348df0442
HokieGeek <andres.f.perez@gmail.com>
2019-02-18 22:35:29 -0500

    Not sure what happened to tapping term, but upped them

--- keyboards/keebio/nyquist/keymaps/ansibles/rules.mk
+++ keyboards/keebio/nyquist/keymaps/ansibles/rules.mk
@@ -2,12 +2,11 @@ USER_NAME := hokiegeek
 
 BOOTLOADER = qmk-dfu
 
-# CONSOLE_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 ENCODER_ENABLE = yes
 BACKLIGHT_ENABLE = yes
-# RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = yes
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
