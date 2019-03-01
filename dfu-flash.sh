becf837c2706f45c6d200b5fd488437e17e18069
HokieGeek <andres.f.perez@gmail.com>
2019-02-28 21:49:15 -0500

    Adding the keymap for my new keeb!

--- dfu-flash.sh
+++ dfu-flash.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-make nyquist/rev2:ansibles:production
+# make keebio/nyquist/rev2:ansibles:production
+make keebio/levinson/rev2:philotes:production
 
 sudo avrdude -c usbtiny -p m32u4 -P usb \
-        -U flash:w:nyquist_rev2_ansibles_production.hex:a \
+        -U flash:w:keebio_levinson_rev2_philotes_production.hex:a \
         -U lfuse:w:0x5E:m -U hfuse:w:0xD9:m -U efuse:w:0xC3:m -U lock:w:0x3F:m
