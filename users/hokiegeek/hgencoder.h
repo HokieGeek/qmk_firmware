4b9de88fdb4c16adb4f962ddad816cb695b296e5
HokieGeek <andres.f.perez@gmail.com>
2019-03-05 09:48:27 -0500

    Cleaned up my userspace and updated my layouts

--- users/hokiegeek/hgencoder.h
+++ users/hokiegeek/hgencoder.h
@@ -1,6 +1,8 @@
 #ifndef USERSPACE_ENCODER
 #define USERSPACE_ENCODER
 
+#include "quantum.h"
+
 #ifndef ENCODER_RESOLUTION
   #define ENCODER_RESOLUTION 4
 #endif
@@ -12,8 +14,12 @@ typedef struct {
     bool defaultVolume;
 } encoder_options;
 
+encoder_options enc_opts;
+
 #define TD_ENCODER_ENTRY [td_encoder] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, encoder_td_actions, NULL, 300)
 
 #define KC_ENC TD(td_encoder)
+
+void hgencoder_init(void);
 
 #endif // USERSPACE_ENCODER
--- users/hokiegeek/taps.c
+++ users/hokiegeek/taps.c
@@ -3,19 +3,19 @@
 #include <mousekey.h>
 #include "quantum.h"
 
-void mousekey_tap(uint8_t code) {
+inline void mousekey_tap(uint8_t code) {
     mousekey_on(code);
     mousekey_send();
     mousekey_off(code);
     mousekey_send();
 }
 
-void key_tap(uint8_t code) {
+inline void key_tap(uint8_t code) {
     register_code(code);
     unregister_code(code);
 }
 
-void tap_ctltab() {
+inline void tap_ctltab() {
     register_code(KC_LCTL);
     register_code(KC_TAB);
     unregister_code(KC_TAB);
