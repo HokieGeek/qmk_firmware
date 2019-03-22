477eca9acc156d3c7e71c43c6057f29c191c3f19
HokieGeek <andres.f.perez@gmail.com>
2019-03-22 08:29:47 -0400

    philotes: added key that kicks off terminal

--- keyboards/keebio/levinson/keymaps/philotes/keymap.c
+++ keyboards/keebio/levinson/keymaps/philotes/keymap.c
@@ -12,7 +12,7 @@ enum custom_keycodes {
 };
 
 // | Lower|      |      |      | GUI  |AltSpc|     |CtlTb | TMUX | ENC  |      | MACR |Raise |
-#define _____BASE_BOTTOM_____  TT(_LOWER),  _______, _______,   _______, LGUI_T(KC_BSPC), LALT_T(KC_SPC),     TD(td_ctltab_extras),  KC_TMUX, KC_ENC,  _______,  KC_SKDM1,  TT(_RAISE)
+#define _____BASE_BOTTOM_____  TT(_LOWER),  _______, _______,   _______, LGUI_T(KC_BSPC), LALT_T(KC_SPC),     TD(td_ctltab_extras),  KC_TMUX, KC_ENC,  G(S(KC_ENT)),  KC_SKDM1,  TT(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
