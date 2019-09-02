54bae08345718059489f862b669650d7e3b8db59
HokieGeek <andres.f.perez@gmail.com>
2019-09-02 11:07:34 -0400

    [hokiegeek] refactored taps to be more consistent and reusable

--- users/hokiegeek/taps.h
+++ users/hokiegeek/taps.h
@@ -1,11 +1,9 @@
 #ifndef USERSPACE_TAPS
 #define USERSPACE_TAPS
 
-void mousekey_tap(uint8_t);
-void key_tap(uint8_t);
+void tap_mousekey(uint8_t);
+void tap_key(uint8_t);
 void tap_mod_key(uint8_t, uint8_t);
-void tap_ctrl_key(uint8_t);
-void tap_ctltab(void);
-void tap_gui_shift_enter(void);
+void tap_mods_key(uint8_t*, uint8_t);
 
 #endif // USERSPACE_TAPS
