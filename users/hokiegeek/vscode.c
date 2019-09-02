5a2f8dca97d1c890f814b9c615ad1a315161025e
HokieGeek <andres.f.perez@gmail.com>
2019-09-01 23:05:16 -0400

    [hokiegeek] greatly updated the vscode bindings now that I am actually using them

--- users/hokiegeek/vscode.c
+++ users/hokiegeek/vscode.c
@@ -2,20 +2,28 @@
 
 #include "keycodes.h"
 
+void vscode_layer_on_hold(td_stage stage) {
+    switch (stage) {
+        case TD_FINISHED: layer_on(_VSCODE); break;
+        case TD_RESET:    layer_off(_VSCODE); break;
+        default: break;
+    }
+}
+
 bool vscode_process_record_user(uint16_t keycode, keyrecord_t *record) {
-    /*
     switch (keycode) {
-        case KC_VSCODE:
+        case VSC_SCM:
             if (record->event.pressed) {
-                layer_on(_VSCODE);
-            } else {
-                layer_off(_VSCODE);
+                register_code(KC_LCTRL);
+                register_code(KC_LSHIFT);
+                register_code(KC_G);
+                unregister_code(KC_G);
+                unregister_code(KC_LSHIFT);
+                unregister_code(KC_LCTRL);
+                register_code(KC_G);
+                unregister_code(KC_G);
             }
             return false;
-        case VSC_TODO:
-            SEND_STRING("TODO");
-            return false;
     }
-    */
     return true;
 }
