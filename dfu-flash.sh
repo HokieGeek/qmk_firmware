2cae0e94dd516d24af3155759d41c560f3184fb5
HokieGeek <andres.f.perez@gmail.com>
2019-09-07 12:52:23 -0400

    [hokiegeek] refactored special codes to have better toggling between linux and mac combos

--- dfu-flash.sh
+++ dfu-flash.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-# make keebio/nyquist/rev2:ansibles:production
-make keebio/levinson/rev2:philotes:production
+make keebio/levinson/rev2:philotes:production || exit 1
 
 sudo avrdude -c usbtiny -p m32u4 -P usb \
         -U flash:w:keebio_levinson_rev2_philotes_production.hex:a \
         -U lfuse:w:0x5E:m -U hfuse:w:0xD9:m -U efuse:w:0xC3:m -U lock:w:0x3F:m
+
+# make keebio/nyquist/rev2:hickory-n-dickory:production || exit 1
+# 
+# sudo avrdude -c usbtiny -p m32u4 -P usb \
+#         -U flash:w:keebio_nyquist_rev2_hickory-n-dickory_production.hex:a \
+#         -U lfuse:w:0x5E:m -U hfuse:w:0xD9:m -U efuse:w:0xC3:m -U lock:w:0x3F:m
