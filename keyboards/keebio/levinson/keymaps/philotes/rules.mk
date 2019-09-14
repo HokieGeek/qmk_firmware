cdc193211e078136a159f446306f40d47789090e
HokieGeek <andres.f.perez@gmail.com>
2019-09-14 17:00:32 -0400

    [hokiegeek] Refactored common stuffs to userspace

--- keyboards/keebio/levinson/keymaps/philotes/rules.mk
+++ keyboards/keebio/levinson/keymaps/philotes/rules.mk
@@ -1,13 +1,4 @@
 USER_NAME := hokiegeek
 
-BOOTLOADER = qmk-dfu
-
-MOUSEKEY_ENABLE = yes
-TAP_DANCE_ENABLE = yes
-ENCODER_ENABLE = yes
 BACKLIGHT_ENABLE = yes
 RGBLIGHT_ENABLE = yes
-
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
