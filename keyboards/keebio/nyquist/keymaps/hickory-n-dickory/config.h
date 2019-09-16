f27a34849176401bb0589a30aa41348a78af3d1a
HokieGeek <andres.f.perez@gmail.com>
2019-09-15 20:22:28 -0400

    [hokiegeek] Cleaning up config redundancies

--- keyboards/keebio/nyquist/keymaps/hickory-n-dickory/config.h
+++ keyboards/keebio/nyquist/keymaps/hickory-n-dickory/config.h
@@ -1,11 +1,4 @@
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#define NO_ACTION_ONESHOT
-
-#include "config_common.h"
+#pragma once
 
 #define ENCODERS_PAD_A { F1 }
 #define ENCODERS_PAD_B { F0 }
-
-#endif
