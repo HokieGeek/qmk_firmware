43c7ce43c5ffdfd818a85490c651a9fb77815500
HokieGeek <andres.f.perez@gmail.com>
2019-10-02 08:43:51 -0400

    [hokiegeek] Fixed the dumb first implementation of the latin code handlers

--- keyboards/keebio/levinson/keymaps/philotes/keymap.c
+++ keyboards/keebio/levinson/keymaps/philotes/keymap.c
@@ -289,7 +289,6 @@ void matrix_init_user(void) {
     userspace_matrix_init_user();
 }
 
-
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
