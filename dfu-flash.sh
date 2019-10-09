3832db7bac0a1c775228bbbdac9453dc60ff283b
HokieGeek <andres.f.perez@gmail.com>
2019-10-09 15:24:44 -0400

    [hokiegeek] Finally got around to making the dfu flasher smarter

--- dfu-flash.sh
+++ dfu-flash.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-make keebio/levinson/rev2:philotes:production || exit 1
+keeb=$1
+
+make ${keeb}:production || exit 1
 
 sudo avrdude -c usbtiny -p m32u4 -P usb \
-        -U flash:w:keebio_levinson_rev2_philotes_production.hex:a \
+        -U flash:w:$(echo ${keeb} | tr '/:' '_')_production.hex:a \
         -U lfuse:w:0x5E:m -U hfuse:w:0xD9:m -U efuse:w:0xC3:m -U lock:w:0x3F:m
-
-# make keebio/nyquist/rev2:hickory-n-dickory:production || exit 1
-# 
-# sudo avrdude -c usbtiny -p m32u4 -P usb \
-#         -U flash:w:keebio_nyquist_rev2_hickory-n-dickory_production.hex:a \
-#         -U lfuse:w:0x5E:m -U hfuse:w:0xD9:m -U efuse:w:0xC3:m -U lock:w:0x3F:m
