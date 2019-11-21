fca1c9f78e79e8b6639f64a464a232b181df88a5
William Chang <william@factual.com>
2019-11-20 23:03:47 -0800

    add layout 2 and rename existing folder to layout 4
    - renamed existing folder with directional layout to layout 4
    - udpated existing readme
    - Added layout_60_2

--- keyboards/dz60/keymaps/billiams_layout2/build_flash.sh
+++ keyboards/dz60/keymaps/billiams_layout2/build_flash.sh
@@ -3,7 +3,7 @@
 # dfu-programmer atmega32u4 reset
 
 # run this in the qmk_firmware directory
-make dz60:billiams
+make dz60:billiams_layout2
 dfu-programmer atmega32u4 erase --force && \
 dfu-programmer atmega32u4 flash dz60_billiams.hex && \
 dfu-programmer atmega32u4 reset
