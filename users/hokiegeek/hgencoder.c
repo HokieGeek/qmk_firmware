a850e07e1d2e342585d47b88943fb935ac104f1e
HokieGeek <andres.f.perez@gmail.com>
2020-02-22 18:29:42 -0500

    [hokiegeek] changed encoder scrolling default back to vertical

--- users/hokiegeek/hgencoder.c
+++ users/hokiegeek/hgencoder.c
@@ -1,7 +1,7 @@
 #include "hgencoder.h"
 
 void hgencoder_init(void) {
-    enc_opts.scrollVertical = false;
+    enc_opts.scrollVertical = true;
     enc_opts.monBrightness = true;
     enc_opts.backlightBrightness = true;
     enc_opts.defaultVolume = true;
