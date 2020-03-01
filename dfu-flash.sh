895fd0bff5fb531f02fcdf94bfc95351577a8f9f
HokieGeek <andres.f.perez@gmail.com>
2020-03-01 18:16:33 -0500

    Added commented feature to dfu flashing script

--- dfu-flash.sh
+++ dfu-flash.sh
@@ -7,3 +7,4 @@ make ${keeb}:production || exit 1
 sudo avrdude -c usbtiny -p m32u4 -P usb \
         -U flash:w:$(echo ${keeb} | tr '/:' '_')_production.hex:a \
         -U lfuse:w:0x5E:m -U hfuse:w:0xD9:m -U efuse:w:0xC3:m -U lock:w:0x3F:m
+        # -U eeprom:w:"quantum/split_common/eeprom-righthand.eep":a
