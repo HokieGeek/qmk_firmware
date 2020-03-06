745b5e41b532b1d63796ef74915769605da3b4be
HokieGeek <andres.f.perez@gmail.com>
2020-03-06 06:32:01 -0500

    [hokiegeek] refactored a bit of my userspace to gain nearly 300 bytes!

--- users/hokiegeek/tmux.h
+++ users/hokiegeek/tmux.h
@@ -19,14 +19,14 @@ enum tmux_custom_keycodes {
 #define SS_TMUX_PREFIX SS_DOWN(X_LCTRL)SS_TAP(TMUX_PREFIX)SS_UP(X_LCTRL)
 #define SEND_TMUX(c) SEND_STRING(SS_TMUX_PREFIX""c);
 
-void td_tmux_finished(qk_tap_dance_state_t *state, void *user_data);
-void td_tmux_reset(qk_tap_dance_state_t *state, void *user_data);
-
-bool tmux_process_record_user(uint16_t keycode, keyrecord_t *record);
-
 #ifndef TMUX_ENTRY_TAPPING_TERM
 #define TMUX_ENTRY_TAPPING_TERM 160
 #endif
 #define TD_TMUX_ENTRY [td_tmux] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, td_tmux_finished, td_tmux_reset, TMUX_ENTRY_TAPPING_TERM)
+
+void td_tmux_finished(qk_tap_dance_state_t *state, void *user_data);
+void td_tmux_reset(qk_tap_dance_state_t *state, void *user_data);
+
+bool tmux_process_record_user(uint16_t keycode, keyrecord_t *record);
 
 #endif // USERSPACE_TMUX
--- users/hokiegeek/vscode.h
+++ users/hokiegeek/vscode.h
@@ -18,7 +18,6 @@
 #define _____VSCODE_R2_____  _______, _______, _______, _______, _______, _______,    _______,      VSC_PKDEFS,  VSC_PKSYMS,  VSC_G2SYM,  VSC_PALETT,  _______
 #define _____VSCODE_R3_____  _______, _______, _______, _______, _______, _______,    VSC_TGL_GTST, VSC_TERM,    VSC_RENAME,  VSC_PROBS,  _______,  _______
 
-void vscode_layer_on_hold(td_stage stage);
 bool vscode_process_record_user(uint16_t keycode, keyrecord_t *record);
 
 #endif // USERSPACE_VSCODE
