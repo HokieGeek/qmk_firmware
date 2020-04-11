e4a2b72d856428fde9f1fa147a7b85b95e9e5520
HokieGeek <andres.f.perez@gmail.com>
2020-04-10 22:09:41 -0400

    Added script to run builds on all of my boards

---
+++ test-keebs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+make keebio/levinson/rev2:philotes || exit 1
+make klaatu/rev1:gort || exit 2
+make keebio/nyquist/rev1:hickory-n-dickory || exit 3
+make keebio/nyquist/rev1:clalack || exit 4
